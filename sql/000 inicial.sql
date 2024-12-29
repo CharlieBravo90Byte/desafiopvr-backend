c4b772363c1246f76aecc5f5f37c9dfb1e7fac17
CharlieBravo90 <ca.bravoj29@gmail.com>
2024-12-29 08:21:14 -0300

    sp create db

---
+++ sql/000 inicial.sql
@@ -0,0 +1,14 @@
+-- Database: db_desafioprevired
+
+DROP DATABASE IF EXISTS db_desafioprevired;
+
+CREATE DATABASE db_desafioprevired
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
--- src/main/java/cl/charlie/desafioprevired/model/Empresa.java
+++ src/main/java/cl/charlie/desafioprevired/model/Empresa.java
@@ -97,9 +97,4 @@ public class Empresa {
         this.trabajadores = trabajadores;
     }
 
-    
-    
-    
-
-    
 }
